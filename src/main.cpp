0e854aa0bfcdd9643a90dfa29c21b4a4f79c8cb6
Adriano Valadar <adriano.valadar@outlook.com>
2024-04-06 12:07:47 +0100

    iterate a given directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <stack>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#define NUMBER_ARGUMENTS 2
+
+int main(int argc, char **argv) {
+    try {
+        std::string directoryToSearch;
+        if (argc != NUMBER_ARGUMENTS)
+            throw std::runtime_error("Number of arguments should be equal to " + std::to_string(NUMBER_ARGUMENTS));
+        directoryToSearch = argv[1];
+        std::stack<std::string> fileNames;
+        for (const auto &entry: std::filesystem::recursive_directory_iterator(directoryToSearch)) {
+            std::cout << entry.path() << std::endl;
+            fileNames.push(entry.path());
+        }
+    } catch (const std::exception &e) {
+        std::cout << "Exception: " << e.what() << std::endl;
+    }
     return 0;
 }
