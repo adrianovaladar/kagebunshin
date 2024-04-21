a707b5ddbdf407ccb8d0a7f8b647e5ff1e33ec65
Adriano Valadar <adriano.valadar@outlook.com>
2024-04-21 23:10:31 +0100

    add implementation for ThreadPool

--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -1,1 +1,27 @@
 #include "ThreadPool.h"
+#include <iostream>
+
+void ThreadPool::worker() {
+    while (true) {
+        Func task;
+        if (!workQueue.pop(task))
+            return;
+        task();
+    }
+}
+
+ThreadPool::ThreadPool(size_t numberThreads) {
+    for (unsigned i = 0; i < numberThreads; i++)
+        threads.emplace_back(&ThreadPool::worker, this);
+}
+
+ThreadPool::~ThreadPool() {
+    workQueue.terminate();
+    for (auto &t: threads) {
+        t.join();
+    }
+}
+
+void ThreadPool::submit(Func f) {
+    workQueue.push(std::move(f));
+}
--- src/ThreadPool.h
+++ src/ThreadPool.h
@@ -1,8 +1,21 @@
 #ifndef KAGEBUNSHIN_THREADPOOL_H
 #define KAGEBUNSHIN_THREADPOOL_H
 
+#include "ConcurrentQueue.h"
+#include <functional>
+#include <thread>
+
+using Func = std::function<void()>;
 
 class ThreadPool {
+    ConcurrentQueue<Func> workQueue;
+    std::vector<std::thread> threads;
+    void worker();
+
+public:
+    explicit ThreadPool(size_t numberThreads = std::thread::hardware_concurrency());
+    ~ThreadPool();
+    void submit(Func f);
 };
 
 
