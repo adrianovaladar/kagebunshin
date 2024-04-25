8fd9b722c9f5befd9fd28927d5b43072d82e5cbf
Adriano Valadar <adriano.valadar@outlook.com>
2024-04-26 00:20:49 +0100

    update tests for ThreadPool

--- tests/ThreadPoolTests.cpp
+++ tests/ThreadPoolTests.cpp
@@ -1,19 +1,53 @@
 #include "ThreadPoolTests.h"
 
-const std::string hello{"Hello from function"};
-
-void function() {
-    logger.log(hello, LOGLEVEL::Info);
-}
-
-TEST_F(ThreadPoolTests, SubmitTest) {
+TEST_F(ThreadPoolTests, SingleTaskExecutionTest) {
     ThreadPool pool{1};
-    pool.submit(function);
+    pool.submit(functionWithArguments);
     sleep(1);
 
     std::stringstream buffer;
     buffer << logFile.rdbuf();
     std::string logContents = buffer.str();
 
-    ASSERT_NE(std::string::npos, logContents.find(hello));
+    ASSERT_NE(std::string::npos, logContents.find(hello) + number);
+}
+
+TEST_F(ThreadPoolTests, ConcurrentTaskExecutionTest) {
+    int numberTimes{10};
+    int count{};
+    ThreadPool pool{10};
+    for (int i{}; i < numberTimes; i++) {
+        pool.submit([this, &count]() {
+            std::scoped_lock lock(mutex);
+            number++;
+            functionWithArguments();
+            count++;
+        });
+    }
+    sleep(1);
+    for (int i{}; i < numberTimes; i++) {
+        logFile.seekg(0, std::ios::beg);
+        std::stringstream buffer;
+        buffer << logFile.rdbuf();
+        std::string logContents = buffer.str();
+        ASSERT_NE(std::string::npos, logContents.find(hello + std::to_string(number)));
+        number--;
+    }
+    ASSERT_EQ(count, numberTimes);
+}
+
+TEST(ThreadPoolTest, SubmitAndExecuteTasks) {
+    int numberTimes{10};
+    ThreadPool pool{4};
+    int sum{};
+    for (int i{}; i < numberTimes; ++i) {
+        pool.submit([i, &sum]() {
+            sum += i;
+        });
+    }
+    sleep(1);
+    for (int i{}; i < numberTimes; ++i) {
+        sum -= i;
+    }
+    ASSERT_EQ(sum, 0);
 }
--- tests/ThreadPoolTests.h
+++ tests/ThreadPoolTests.h
@@ -5,14 +5,24 @@
 #include "../src/ThreadPool.h"
 #include <gtest/gtest.h>
 
+const std::string hello{"Hello from function "};
+
+void function(int number) {
+    logger.log(hello + std::to_string(number), LOGLEVEL::Info);
+}
+
 class ThreadPoolTests : public ::testing::Test {
 protected:
+    std::function<void()> functionWithArguments;
     std::ifstream logFile;
+    int number{};
+    std::mutex mutex;
     void SetUp() override {
         logFile.open(logger.getLogFileName());
         if (!logFile.is_open()) {
             FAIL() << "Failed to open log file: " << logger.getLogFileName();
         }
+        functionWithArguments = [this]() { return function(number); };
     }
 
     void TearDown() override {
