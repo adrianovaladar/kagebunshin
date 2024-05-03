937a1d31268213fe786b27d3a78384fa04f26515
Adriano Valadar <adriano.valadar@outlook.com>
2024-05-04 00:21:42 +0100

    update tests for TextFinder

--- tests/TextFinderTests.cpp
+++ tests/TextFinderTests.cpp
@@ -4,3 +4,18 @@ TEST_F(TextFinderTests, FindWithDirectoryThatDoesNotExist) {
     tf.find("/directory/that/does/not/exist", {"text"});
     ASSERT_EQ(tf.getFoundFiles().size(), 0);
 }
+
+TEST_F(TextFinderTests, FindWithoutWords) {
+    tf.find(std::string(path), {});
+    ASSERT_EQ(tf.getFoundFiles().size(), 0);
+}
+
+TEST_F(TextFinderTests, FindSuccessfully) {
+    tf.find(std::string(path), {text});
+    ASSERT_GE(tf.getFoundFiles().size(), 1);
+}
+
+TEST_F(TextFinderTests, FindUnsuccessfully) {
+    tf.find(std::string(path), {text + "abc"});
+    ASSERT_EQ(tf.getFoundFiles().size(), 0);
+}
--- tests/TextFinderTests.h
+++ tests/TextFinderTests.h
@@ -3,18 +3,22 @@
 
 #include <gtest/gtest.h>
 #include "../src/TextFinder.h"
+#include <fstream>
 
 class TextFinderTests : public ::testing::Test {
 protected:
     TextFinder tf;
+    std::filesystem::path path {std::filesystem::current_path() / "directory"};
+    std::string text  {"hello"};
     void SetUp() override {
-
+        std::filesystem::create_directory(path);
+        std::ofstream outputFile("directory/example.txt");
+        outputFile << text;
     }
 
     void TearDown() override {
-
+        std::filesystem::remove_all(path);
     }
-
 };
 
 
