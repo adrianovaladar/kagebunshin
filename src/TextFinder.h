8cee426f8774c38676e604a05a1bb3588cfce4b3
Adriano Valadar <adriano.valadar@outlook.com>
2024-05-05 23:17:20 +0100

    add Doxygen comments to document TextFinder

--- src/TextFinder.h
+++ src/TextFinder.h
@@ -4,18 +4,38 @@
 #include <filesystem>
 #include <stack>
 #include <vector>
-
+/**
+ * @brief Class for finding text occurrences within files in a directory.
+ */
 class TextFinder {
-    std::filesystem::path directory;
-    std::vector<std::string> words;
-    std::stack <std::filesystem::path> foundFiles;
+    std::filesystem::path directory;/**< Directory to search.*/
+    std::vector<std::string> words;/**< Words to search.*/
+    std::stack <std::filesystem::path> foundFiles;/**< Stack that contains the files where the words were found.*/
+    /** @brief Find the words in the directory.
+     */
     void find();
+    /** @brief Find the words in a given file.
+    *   @param files Stack that contains the files to search.
+    */
     void findInFile(std::stack<std::filesystem::path> &files);
 
 public:
+    /**
+     * Default constructor.
+     */
     explicit TextFinder() = default;
+    /**
+     * Getter of the found files.
+     * @return Stack that contains the found files.
+     */
     [[nodiscard]] std::stack<std::filesystem::path> getFoundFiles() const { return foundFiles; }
+    /**
+     * @brief Find the words in the directory.
+     * @param directoryToSearch directory to search.
+     * @param wordsToSearch words to search.
+     */
     void find(std::filesystem::path &&directoryToSearch, std::vector<std::string> &&wordsToSearch);
 };
 
 #endif//KAGEBUNSHIN_TEXTFINDER_H
+
